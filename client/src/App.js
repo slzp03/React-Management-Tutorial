f4d58766018cbe4ae8b4511b39c0477e72b6cd91
slzp03 <slzp03@naver.com>
2020-11-04 17:21:44 +0900

    Add Get Customers REST API

--- client/src/App.js
+++ client/src/App.js
@@ -20,33 +20,25 @@ const styles = theme=>({
   }
 })
 
-const customers = [{
-  'id' : 1,
-  'image' : 'http://placeimg.com/64/64/1',
-  'name' : '김명훈',
-  'birthday' : '951123',
-  'gender' : '남',
-  'job' : '무직'
-},
-{
-  'id' : 2,
-  'image' : 'http://placeimg.com/64/64/2',
-  'name' : '김다훈',
-  'birthday' : '940513',
-  'gender' : '남',
-  'job' : '직장인'
-},
-{
-  'id' : 3,
-  'image' : 'http://placeimg.com/64/64/3',
-  'name' : '김성기',
-  'birthday' : '571009',
-  'gender' : '남',
-  'job' : '직장인'
-}
-]
-
 class App extends Component{
+
+  state = {
+    customers:""
+
+  }
+
+  componentDidMount(){
+    this.callApi()
+      .then(res => this.setState({customers:res}))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async()=>{
+    const response = await fetch('/api/customers');
+    const body = await response.json();
+    return body;
+  }
+
   render(){
     const {classes}=this.props;
     return(
@@ -63,7 +55,9 @@ class App extends Component{
             </TableRow>
           </TableHead>
           <TableBody>
-          { customers.map(c => {  return (  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})}
+          { this.state.customers ? this.state.customers.map(c => { 
+            return (  <Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job}/>);})
+          : "" }
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/hello',(req,res)=>{
-    res.send({message:'Hello Express!'});
-});
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+        'id' : 1,
+        'image' : 'http://placeimg.com/64/64/1',
+        'name' : '김명훈',
+        'birthday' : '951123',
+        'gender' : '남',
+        'job' : '무직'
+        },
+        {
+        'id' : 2,
+        'image' : 'http://placeimg.com/64/64/2',
+        'name' : '김다훈',
+        'birthday' : '940513',
+        'gender' : '남',
+        'job' : '직장인'
+        },
+        {
+        'id' : 3,
+        'image' : 'http://placeimg.com/64/64/3',
+        'name' : '인화심',
+        'birthday' : '611130',
+        'gender' : '여',
+        'job' : '직장인'
+        }
+        ])
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

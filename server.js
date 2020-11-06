3b88b0f36b560be1b343c6530edd79a0f461daa3
slzp03 <slzp03@naver.com>
2020-11-06 17:24:35 +0900

    Create Customer Database Table

--- server.js
+++ server.js
@@ -2,37 +2,33 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const fs = require('fs');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: conf.host,
+    user:conf.user,
+    password:conf.password,
+    port:conf.port,
+    database:conf.database
+});
+
+connection.connect();
+
+
 app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-        'id' : 1,
-        'image' : 'http://placeimg.com/64/64/1',
-        'name' : '김명훈',
-        'birthday' : '951123',
-        'gender' : '남',
-        'job' : '무직'
-        },
-        {
-        'id' : 2,
-        'image' : 'http://placeimg.com/64/64/2',
-        'name' : '김다훈',
-        'birthday' : '940513',
-        'gender' : '남',
-        'job' : '직장인'
-        },
-        {
-        'id' : 3,
-        'image' : 'http://placeimg.com/64/64/3',
-        'name' : '인화심',
-        'birthday' : '611130',
-        'gender' : '여',
-        'job' : '직장인'
+    connection.query(
+        "select * from customer",
+        (err, rows, fields) => {
+            res.send(rows);
         }
-        ])
-})
+    );
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
